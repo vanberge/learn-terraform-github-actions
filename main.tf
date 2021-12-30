3a5443062d570852f66c3d0ae56513eaaba5311f
Eric VanBergen <vanberge@gmail.com>
2021-12-30 11:39:03 -0500

    unvariablizes TF vars

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = var.TF_ORG
+    organization = "evb-sandbox"
 
     workspaces {
-      name = var.TF_WORKSPACE
+      name = "evb-gcp-sandbox"
     }
   }
 }
