590e5a523e5d92f05fa01d1cbfdf69f61e621341
Eric VanBergen <vanberge@gmail.com>
2022-01-05 11:18:35 -0500

    subnet back in

--- main.tf
+++ main.tf
@@ -108,7 +108,7 @@ resource "google_container_cluster" "primary" {
   remove_default_node_pool = true
   initial_node_count       = 1
   network                  = "${var.GCP_VPC}-k8s"
-  //subnetwork               = "${var.GCP_VPC}-k8s-sn"
+  subnetwork               = "${var.GCP_VPC}-k8s-sn"
 }
 
 resource "google_container_node_pool" "primary_nodes" {
