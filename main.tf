91d16f2c9ed210deffa81f70147a0d310f2125a2
Eric VanBergen <vanberge@gmail.com>
2022-01-26 16:28:23 -0500

    remove the comment
    comment brb

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     google = {
-      source  = "hashicorp/google" #add a comment
+      source  = "hashicorp/google"
       version = "3.5.0"
     }
     random = {
