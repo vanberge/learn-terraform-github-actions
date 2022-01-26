b63fe282658ad6b357d865e5014d488088d67460
Eric VanBergen <vanberge@gmail.com>
2022-01-26 16:21:39 -0500

    Adding a comment
    straight to main

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     google = {
-      source  = "hashicorp/google"
+      source  = "hashicorp/google" #add a comment
       version = "3.5.0"
     }
     random = {
